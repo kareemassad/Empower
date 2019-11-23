25033d3a6f2dba9ea829964e58853aa56d53be0f
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-11-23 13:15:19 -0500

    Actual modifications, last commit was a dud

---
+++ app.py
@@ -0,0 +1,49 @@
+
+
+from flask import Flask, render_template, request
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map, icons
+
+app = Flask(__name__, template_folder="templates")
+
+# you can also pass key here
+GoogleMaps(
+    app,
+    key=" AIzaSyDzLfe1r1aV48C15pGg_-PI0m0upPwFi3U"
+)
+
+
+@app.route("/")
+def mapview():
+    lat = 40.7128
+    long = 74.0060
+
+    circlemap = Map(
+        identifier="circlemap",
+        varname="circlemap",
+        lat=lat,
+        lng=-long,
+        markers=[
+            {
+                'icon': '//maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': lat,
+                'lng': -long,
+                'infobox': "Hello I am <b style='color:green;'>GREEN</b>!"
+            }
+
+        ],
+        style=(
+            "height:500px;"
+            "width:80%;"
+                ),
+    )
+
+    return render_template(
+        'nearMe.html',
+        circlemap=circlemap,
+        GOOGLEMAPS_KEY=request.args.get('apikey'),
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, use_reloader=True)
